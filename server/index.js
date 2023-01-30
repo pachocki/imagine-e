067320135610fcfc3e4bfa55fb701f87d85946e4
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-01-30 09:46:21 +0100

    updt

--- server/index.js
+++ server/index.js
@@ -9,13 +9,13 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(cors({ origin: 'https://imagine-owgvdxrgn-pachocki.vercel.app' }));
+app.use(express.json({ limit: "50mb" }));
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).json({
     message: 'Hello from DALL.E!',
   });
