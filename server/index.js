526e8d13988c944665104f8444fd45d7ec098604
Wojciech Pachocki <wojtek.kodehode@gmail.com>
2023-01-30 12:03:04 +0100

    updt

--- server/index.js
+++ server/index.js
@@ -1,30 +1,38 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
-import connectDB from './mongodb/connect.js';
-import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/dalleRoutes.js';
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: 'https://imagine-owgvdxrgn-pachocki.vercel.app' }));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/dalle', dalleRoutes);
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.status(200).json({
-    message: 'Hello from DALL.E!',
+    message: "Hello from DALL.E!",
   });
 });
 
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(5000, () => console.log('Server started on port 5000'));
+    app.listen(5000, () => console.log("Server started on port 5000"));
   } catch (error) {
     console.log(error);
   }
